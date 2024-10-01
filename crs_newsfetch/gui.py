040ee5d1822c6afa6783d813feb659956b7bb8b2
Lavender Perry <lavender.perry@outlook.com>
2024-10-01 16:27:53 -0700

    Add scrollable container for results

--- crs_newsfetch/gui.py
+++ crs_newsfetch/gui.py
@@ -8,38 +8,44 @@ class Gui(QtWidgets.QWidget):
         super().__init__()
 
         self.layout = QtWidgets.QVBoxLayout(self)
+        layoutWidget = self.layout.widget()
 
         today = datetime.date.today()
         lastMonth = today.replace(day = 1) - datetime.timedelta(days = 1)
         if lastMonth.day > today.day:
             lastMonth = lastMonth.replace(day = today.day)
 
-        startDateLayout = QtWidgets.QHBoxLayout(self)
+        startDateLayout = QtWidgets.QHBoxLayout(layoutWidget)
         startDateEdit = QtWidgets.QDateTimeEdit(lastMonth, calendarPopup = True)
         startDateLabel = QtWidgets.QLabel("Start date:")
         startDateLabel.setBuddy(startDateEdit)
         startDateLayout.addWidget(startDateLabel)
         startDateLayout.addWidget(startDateEdit)
 
-        endDateLayout = QtWidgets.QHBoxLayout(self)
+        endDateLayout = QtWidgets.QHBoxLayout(layoutWidget)
         endDateEdit = QtWidgets.QDateTimeEdit(today, calendarPopup = True)
         endDateLabel = QtWidgets.QLabel("End date:")
         endDateLabel.setBuddy(endDateEdit)
         endDateLayout.addWidget(endDateLabel)
         endDateLayout.addWidget(endDateEdit)
 
-        self._resultsLayout = QtWidgets.QVBoxLayout(self)
+        resultsScrollArea = QtWidgets.QScrollArea(layoutWidget)
+        resultsWidget = QtWidgets.QWidget(resultsScrollArea)
+        self._resultsLayout = QtWidgets.QVBoxLayout(resultsWidget)
+        resultsWidget.setLayout(self._resultsLayout)
+        resultsScrollArea.setWidgetResizable(True)
+        resultsScrollArea.setWidget(resultsWidget)
 
         self.layout.addWidget(QtWidgets.QLabel("Please choose a date range to search",
                                                alignment = QtCore.Qt.AlignCenter))
         self.layout.addLayout(startDateLayout)
         self.layout.addLayout(endDateLayout)
 
-        searchButton = QtWidgets.QPushButton("Search", self)
+        searchButton = QtWidgets.QPushButton("Search", layoutWidget)
         searchButton.clicked.connect(self._onSearchClick)
         self.layout.addWidget(searchButton)
 
-        self.layout.addLayout(self._resultsLayout)
+        self.layout.addWidget(resultsScrollArea)
 
     def _onSearchClick(self):
         # This is currently just a test until we have actual data
@@ -52,11 +58,11 @@ class Gui(QtWidgets.QWidget):
         self._addResult(testResult)
 
     def _addResult(self, result: ScholarResult):
-        resultFrame = QtWidgets.QFrame(self)
+        resultFrame = QtWidgets.QFrame(self._resultsLayout.widget())
         resultFrame.setLineWidth(2)
         resultFrame.setFrameStyle(QtWidgets.QFrame.Box | QtWidgets.QFrame.Plain)
 
-        resultBox = QtWidgets.QVBoxLayout(self)
+        resultBox = QtWidgets.QVBoxLayout(resultFrame)
 
         resultBox.addWidget(QtWidgets.QLabel(result.title))
         resultBox.addWidget(QtWidgets.QLabel(result.professor))
