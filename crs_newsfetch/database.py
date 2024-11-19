aeb154d7d360911c3dabf7eb4d67f4f9dee2be13
Agood311 <157413856+Agood311@users.noreply.github.com>
2024-11-19 10:33:12 -0800

    Update database.py

--- crs_newsfetch/database.py
+++ crs_newsfetch/database.py
@@ -6,6 +6,12 @@ class DatabaseManager:
         self.cursor = self.conn.cursor()
         self.create_tables()
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close_connection()
+
     def create_tables(self):
         self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS authors (
@@ -13,7 +19,7 @@ class DatabaseManager:
             name TEXT UNIQUE NOT NULL
         )
         ''')
-
+        
         self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS publications (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,15 +27,14 @@ class DatabaseManager:
             title TEXT NOT NULL,
             year INTEGER,
             url TEXT,
-            form_submitted INTEGER DEFAULT 0,  
+            form_submitted INTEGER DEFAULT 0,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            is_published INTEGER DEFAULT 0,
             FOREIGN KEY (author_id) REFERENCES authors (id)
         )
         ''')
-        #PRAGMA table_info(publications); 
-
         self.conn.commit()
 
-    #try catch to insert author table
     def insert_author(self, author_name):
         try:
             self.cursor.execute('INSERT OR IGNORE INTO authors (name) VALUES (?)', (author_name,))
@@ -40,25 +45,16 @@ class DatabaseManager:
             print(f"Error inserting author!!! {e}")
             return None
 
-    # try catch to insert publication table
-    def insert_publication(self, author_id, title, year, url, form_submitted=0):
+    def insert_publication(self, author_id, title, year, url, form_submitted=0, is_published=0):
         try:
             self.cursor.execute('''
-            INSERT INTO publications (author_id, title, year, url, form_submitted)
-            VALUES (?, ?, ?, ?, ?)
-            ''', (author_id, title, year, url, form_submitted))
+            INSERT INTO publications (author_id, title, year, url, form_submitted, is_published)
+            VALUES (?, ?, ?, ?, ?, ?)
+            ''', (author_id, title, year, url, form_submitted, is_published))
             self.conn.commit()
         except Exception as e:
             print(f"Error inserting publication!!! {e}")
             return None
-
-    def close_connection(self):
-        self.conn.close()
-    
-    # note to code reviwers-- you can check strucuture easily by running an sql script
-    # i.e. "SELECT * FROM authors;" and "SELECT * FROM publications;" 
-    # make sure to populate database using scraper. get scraper code. write author names in a txt file called names.txt
-    # hopefully saves time
 
     def get_all_authors(self):
         self.cursor.execute('SELECT * FROM authors')
@@ -68,9 +64,10 @@ class DatabaseManager:
             print(author)
 
     def get_all_publications(self):
-        # retrives assosiated authors and publications using unique author id as a foreign key
         self.cursor.execute('''
-            SELECT publications.id, authors.name, publications.title, publications.year, publications.url, publications.form_submitted
+            SELECT publications.id, authors.name, publications.title, publications.year, 
+                   publications.url, publications.form_submitted, publications.timestamp, 
+                   publications.is_published
             FROM publications
             JOIN authors ON publications.author_id = authors.id
         ''')
@@ -78,3 +75,12 @@ class DatabaseManager:
         print("Publications:")
         for publication in publications:
             print(publication)
+
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+
+# note to code reviwers-- you can check strucuture easily by running an sql script
+    # i.e. "SELECT * FROM authors;" and "SELECT * FROM publications;" 
+    # make sure to populate database using scraper. get scraper code. write author names in a txt file called names.txt
+    # hopefully saves time
